7c5278da08369c3563803d34229174e4160355fb
SIMRAN-089 <159263244+SIMRAN-089@users.noreply.github.com>
2026-07-21 19:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (88.52%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int pivot = findpivotwithduplicates(nums);
+        return nums[pivot+1];
+    }
+    public int findpivot(int[]nums){
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mid<end && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            if(mid > start && nums[mid]<nums[mid-1]){
+                return mid-1;
+            }
+            if(nums[mid]<nums[start]){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+        public int findpivotwithduplicates(int[]nums){
+            int start = 0;
+            int end = nums.length-1;
+            while(start<=end){
+                int mid = start + (end-start)/2;
+            if(mid < end && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            if(mid > start && nums[mid]<nums[mid-1]){
+                return mid-1;
+            }
+            if(nums[mid]==nums[start]&&nums[mid]==nums[end]){
+                if(start < end && nums[start]>nums[start+1]){
+                    return start;
+                }
+                start++;
+            if(end > start && nums[end]<nums[end-1]){
+                return end-1;
+            }
+            end--;
+            }
+            else if(nums[start]<nums[mid]||nums[start]==nums[mid]&&nums[mid]>nums[end]){
+                start = mid+1;
+            } else{
+                end = mid-1;
+            }
+        
+        }
+        return -1;
+    }
+}
