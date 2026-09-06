45c674bd5220157d6afd05f8e538ac2548acd75a
Simran Singh <159263244+SIMRAN-089@users.noreply.github.com>
2026-09-06 23:59:40 +0530

    Time: 2 ms (99.36%) | Memory: 47.1 MB (59.13%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -5,10 +5,12 @@ class Solution {
       Map<Integer,Integer> map = new HashMap<>();
       for(int i=0;i<nums.length;i++){
         int remain = target - nums[i];
-        if(map.containsKey(remain)){
+        if(map.containsKey(remain)){//if remain exist in map then we return
             return new int[]{i,map.get(remain)};
         }
-        map.put(nums[i],i);
+        map.put(nums[i],i);//otherwise we put it in the map
+        // {2,7,11,15} 
+        // map -> 2:0, 7 
       }
       return null;
       
